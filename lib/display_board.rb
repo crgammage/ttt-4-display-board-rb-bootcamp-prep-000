35295da5a597998c07393cc1fed0ccb852a95d60
masterful-array-3850 <crgammage@gmail.com>
2020-04-13 19:40:40 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -7,31 +7,31 @@ puts "-----------"
 puts "#{board[6]}|#{board[7]}|#{board[8]}"
 end
 
-puts "Turn 1"
-rows = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
-display_board(rows)
-
-puts "Turn 2"
-board = ["O", " ", " ", " ", " ", " ", " ", " ", " "]
-display_board(board)
-
-board = ["O", " ", " ", " ", "X", " ", " ", " ", " "]
-display_board(board)
-
-board = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
-display_board(board)
-
-board = [" ", " ", " ", " ", " ", " ", "O", "O", "O"]
-display_board(board)
-
-board = ["X", " ", " ", " ", "X", " ", " ", " ", "X"]
-display_board(board)
-
-board = [" ", " ", "O", " ", "O", " ", "O", " ", " "]
-display_board(board)
-
-board = ["X", "X", "X", "X", "X", "X", "X", "X", "X"]
-display_board(board)
-
-board = ["X", "X", "X", "X", "X", "X", "X", "X", "X"]
-display_board(board)
+# puts "Turn 1"
+# rows = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
+# display_board(rows)
+#
+# puts "Turn 2"
+# board = ["O", " ", " ", " ", " ", " ", " ", " ", " "]
+# display_board(board)
+#
+# board = ["O", " ", " ", " ", "X", " ", " ", " ", " "]
+# display_board(board)
+#
+# board = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
+# display_board(board)
+#
+# board = [" ", " ", " ", " ", " ", " ", "O", "O", "O"]
+# display_board(board)
+#
+# board = ["X", " ", " ", " ", "X", " ", " ", " ", "X"]
+# display_board(board)
+#
+# board = [" ", " ", "O", " ", "O", " ", "O", " ", " "]
+# display_board(board)
+#
+# board = ["X", "X", "X", "X", "X", "X", "X", "X", "X"]
+# display_board(board)
+#
+# board = ["X", "X", "X", "X", "X", "X", "X", "X", "X"]
+# display_board(board)
