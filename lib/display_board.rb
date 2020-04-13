6ee8c591c98d60054215d8f024d942acb6279c3a
masterful-array-3850 <crgammage@gmail.com>
2020-04-13 19:11:15 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -7,12 +7,3 @@ puts "   |   |   "
 puts "-----------"
 puts "   |   |   "
 end
-
-board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
-def display_board(board)
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |#{board[4]}|   "
-  puts "-----------"
-  puts "   |   |   "
-  end
